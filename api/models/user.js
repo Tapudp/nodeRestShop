e9be1f742bb93fa1fa2557cfc0c48e312e4389a9
Tapudp <divyeshpar2496@gmail.com>
2018-03-24 18:53:09 +0530

    User login and JWT signing logged in with bcrypy.compare and token = jwt.sign()

--- api/models/user.js
+++ api/models/user.js
@@ -4,9 +4,9 @@ const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     email: { type: String,
         required: true,
-        unique: true, 
+        unique: true, // unique doesn't give out a valdation but it just stores that, if we add the match email regex it will only accept email format strings in there.
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ 
-    }, // unique doesn't give out a valdation but it jsut
+    }, 
     password: { type: String, required: true }
 });
 
